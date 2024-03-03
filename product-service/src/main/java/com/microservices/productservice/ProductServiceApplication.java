f88651b23b15271a6aba4897203107ecb1eabec2
bniculea <bogdan.niculeasa@hotmail.com>
2024-03-03 22:18:58 +0200

    fix name typo

--- product-service/src/main/java/com/microservices/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/productservice/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microservices.demo;
+package com.microservices.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- product-service/src/test/java/com/microservices/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/microservices/productservice/ProductServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microservices.demo;
+package com.microservices.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
